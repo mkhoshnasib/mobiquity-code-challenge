af1373d8e86afbe454a485cad89210427218f106
Majid Khoshnasib <majid.khoshnasib@gmail.com>
2018-09-15 12:26:17 +0430

    http-client.test.js improved.

--- src/services/http/http-client/http-client.test.js
+++ src/services/http/http-client/http-client.test.js
@@ -1,5 +1,5 @@
 import httpClient from './http-client';
 
 it('httpClient get call works', () => {
-  httpClient.getCall('/f1').then(res => expect(res.status).toBe(200));
+  httpClient.getCall('').then(res => expect(res.status).toBe(200));
 });
