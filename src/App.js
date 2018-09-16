7a4be085a5147da83981b6aa9123e8d5e85e9267
Majid Khoshnasib <majid.khoshnasib@gmail.com>
2018-09-16 17:58:07 +0430

    Description about decisions added to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import WorldChampionsList from './components/WorldChampionsList/WorldChampionsLi
 import SeasonWinners from './components/SeasonWinnersList/SeasonWinnersList';
 
 /** clean code is it's own documentation */
+
+/** I usually use a combination of functional and OOP
+ *  when I'm working with React. In my opinion using OOP
+ *  in components is more readable and easy to maintain,
+ *  although I use functional style in Redux integration
+ *  (defining store, reducers and actions using pure functions),
+ *  because the purpose of using Redux is immutability and functional
+ *  styles considers a lot about immutability. */
+
 class App extends Component {
   render() {
     return (
